843b432ebf46256f11cc1caf7f07500f3c6449d1
diego <dcervantesapa@unsa.edu.pe>
2026-06-02 13:11:32 -0500

    corrigiendo ruta (si no se cambia no funciona la ejecución) de index.html

--- destinos_arequipa/views.py
+++ destinos_arequipa/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 
-//Vista para la pagina de inicio, se renderiza el template index.html
+#Vista para la pagina de inicio, se renderiza el template index.html
 def index (request):
-    return render(request, "destinos_arequipa/index.html")
+    return render(request, "index.html")
