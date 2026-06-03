f25a67190cb9348681aab55a3ea4abd9faa2f7aa
diego <dcervantesapa@unsa.edu.pe>
2026-06-03 16:01:46 -0500

    cambiando a $ {{price}} para usar el principio dinamico de django con python
    Usamos el simbolo $ {{price}} pues es una variable de Django que sirve para mostrar el precio dinámicamente

--- destinos_arequipa/views.py
+++ destinos_arequipa/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 #Vista para la pagina de inicio, se renderiza el template index.html
 def index (request):
-    return render(request, "index.html")
+    return render(request, "index.html", {'price': 100})
