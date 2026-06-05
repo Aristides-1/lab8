adb1e34c7d54203e25958502e672d65d3fc4b6ca
diego <dcervantesapa@unsa.edu.pe>
2026-06-04 23:00:11 -0500

    añadiendo CRUD para urls.py
    para destinos turisticos agregado CRUD

--- destinos_arequipa/urls.py
+++ destinos_arequipa/urls.py
@@ -1,5 +1,30 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [   path("",views.index, name="index"   ),
+urlpatterns = [
+
+    path(
+        "",
+        views.index,
+        name="index"
+    ),
+
+    path(
+        "crear/",
+        views.crear_destino,
+        name="crear_destino"
+    ),
+
+    path(
+        "editar/<int:id>/",
+        views.editar_destino,
+        name="editar_destino"
+    ),
+
+    path(
+        "eliminar/<int:id>/",
+        views.eliminar_destino,
+        name="eliminar_destino"
+    ),
+
 ]
