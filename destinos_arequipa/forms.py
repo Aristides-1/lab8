555b8cd6eeff717e59b8adff8928be264fd96f9b
diego <dcervantesapa@unsa.edu.pe>
2026-06-04 22:57:49 -0500

    configurando views.py
    reordenando codigo para agregar vista a CRUD, con forms.py y views.py

--- destinos_arequipa/forms.py
+++ destinos_arequipa/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 from .models import DestinoTuristico
 
-class DestinoForm(forms.ModelForm):
+
+class DestinoTuristicoForm(forms.ModelForm):
+
     class Meta:
         model = DestinoTuristico
-        fields = '__all__'
+
+        fields = [
+            'nombreCiudad',
+            'descripcionCiudad',
+            'imagenCiudad',
+            'precioTour',
+            'ofertaTour'
+        ]
--- destinos_arequipa/views.py
+++ destinos_arequipa/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import DestinoTuristico
+from .forms import DestinoTuristicoForm
+
 
 def index(request):
+
     destinos = DestinoTuristico.objects.all()
 
     return render(
@@ -12,3 +15,77 @@ def index(request):
             "destinos": destinos
         }
     )
+
+
+def crear_destino(request):
+
+    if request.method == "POST":
+
+        form = DestinoTuristicoForm(
+            request.POST,
+            request.FILES
+        )
+
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+
+    else:
+
+        form = DestinoTuristicoForm()
+
+    return render(
+        request,
+        "form_destino.html",
+        {
+            "form": form,
+            "titulo": "Agregar Destino"
+        }
+    )
+
+
+def editar_destino(request, id):
+
+    destino = get_object_or_404(
+        DestinoTuristico,
+        id=id
+    )
+
+    if request.method == "POST":
+
+        form = DestinoTuristicoForm(
+            request.POST,
+            request.FILES,
+            instance=destino
+        )
+
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+
+    else:
+
+        form = DestinoTuristicoForm(
+            instance=destino
+        )
+
+    return render(
+        request,
+        "form_destino.html",
+        {
+            "form": form,
+            "titulo": "Editar Destino"
+        }
+    )
+
+
+def eliminar_destino(request, id):
+
+    destino = get_object_or_404(
+        DestinoTuristico,
+        id=id
+    )
+
+    destino.delete()
+
+    return redirect("index")
